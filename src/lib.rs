eb29f9f4712c59e43ca4ae4afbf2695f5cc26a95
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-10-17 19:40:42 +0530

    Add pantry ingredients

--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,31 @@ impl<'r> Responder<'r, 'static> for Ingredients {
     }
 }
 
+/// Struct to represent the expiry and quantity
+/// of an ingredient in the pantry.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PantryIngredient {
+    /// Name of the ingredient.
+    pub name: String,
+    /// Quantity of the ingredient in grams.
+    pub quantity: f32,
+    /// Expiry date of the ingredient in number of days.
+    pub expiry: i32,
+}
+
+impl<'r> Responder<'r, 'static> for PantryIngredient {
+    fn respond_to(self, _: &rocket::Request) -> rocket::response::Result<'static> {
+        rocket::Response::build()
+            .header(rocket::http::ContentType::JSON)
+            .sized_body(
+                serde_json::to_string(&self).unwrap().len(),
+                std::io::Cursor::new(serde_json::to_string(&self).unwrap()),
+            )
+            .ok()
+    }
+}
+
+/// Ingredient struct.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Ingredient {
     /// Name of the ingredient.
